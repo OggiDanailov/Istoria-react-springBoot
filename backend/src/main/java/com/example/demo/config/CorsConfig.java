0f109bda75b904cea00776a16e4120e81003165f
oxd017 <oxd017@jefferson.edu>
2026-01-16 10:38:47 -0500

    adding avecaesar to the config urls

--- backend/src/main/java/com/example/demo/config/CorsConfig.java
+++ backend/src/main/java/com/example/demo/config/CorsConfig.java
@@ -9,7 +9,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost", "http://localhost:80", "http://localhost:5173", "http://45.55.34.96")
+                .allowedOrigins("http://localhost", "http://localhost:80", "http://localhost:5173", "http://45.55.34.96", "http://avecaesar.org",
+                "http://www.avecaesar.org")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
